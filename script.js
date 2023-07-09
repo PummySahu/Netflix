56e88e3eee6676ef0fd8bf87ec5f97141db4d6bf
PummySahu <127649478+PummySahu@users.noreply.github.com>
2023-07-09 12:28:04 +0530

    Create js

---
+++ script.js
@@ -0,0 +1,88 @@
+flag =0
+document.querySelector("#main6>#box1").addEventListener("click",function(){
+    if(flag==0){
+    document.querySelector("#main6>#box1>h1").style.rotate = "45deg",
+    
+    document.querySelector("#main6>#ans1").style.maxHeight =  "30%",
+    document.querySelector("#main6>#ans1").style.visibility = "visible",
+flag=1
+    }
+    else{
+        document.querySelector("#main6>#box1>h1").style.rotate = "0deg",
+        document.querySelector("#main6>#ans1").style.maxHeight =  "0%",
+        document.querySelector("#main6>#ans1").style.visibility = "hidden",
+        flag=0
+    }
+})
+document.querySelector("#main6>#box2").addEventListener("click",function(){
+    if(flag==0){
+    document.querySelector("#main6>#box2>h1").style.rotate = "45deg",
+    document.querySelector("#main6>#ans2").style.maxHeight =  "30%",
+    document.querySelector("#main6>#ans2").style.visibility = "visible",
+    flag=1
+    }
+    else{
+        document.querySelector("#main6>#box2>h1").style.rotate = "0deg",
+        document.querySelector("#main6>#ans2").style.maxHeight =  "0%",
+        document.querySelector("#main6>#ans2").style.visibility = "hidden",
+        flag=0
+    }
+})
+document.querySelector("#main6>#box3").addEventListener("click",function(){
+    if(flag==0){
+    document.querySelector("#main6>#box3>h1").style.rotate = "45deg",
+    document.querySelector("#main6>#ans3").style.maxHeight =  "30%",
+    document.querySelector("#main6>#ans3").style.visibility = "visible",
+    flag=1
+    }
+    else{
+        document.querySelector("#main6>#box3>h1").style.rotate = "0deg",
+        document.querySelector("#main6>#ans3").style.maxHeight =  "0%",
+        document.querySelector("#main6>#ans3").style.visibility = "hidden",
+        flag=0
+    }
+})
+document.querySelector("#main6>#box4").addEventListener("click",function(){
+    if(flag==0){
+    document.querySelector("#main6>#box4>h1").style.rotate = "45deg",
+    document.querySelector("#main6>#ans4").style.maxHeight =  "30%",
+    document.querySelector("#main6>#ans4").style.visibility = "visible",
+    flag=1
+    }
+    else{
+        document.querySelector("#main6>#box4>h1").style.rotate = "0deg",
+        document.querySelector("#main6>#ans4").style.maxHeight =  "0%",
+        document.querySelector("#main6>#ans4").style.visibility = "hidden",
+        flag=0
+    }
+})
+document.querySelector("#main6>#box5").addEventListener("click",function(){
+    if(flag==0){
+    document.querySelector("#main6>#box5>h1").style.rotate = "45deg",
+    document.querySelector("#main6>#ans5").style.maxHeight =  "30%",
+    document.querySelector("#main6>#ans5").style.visibility = "visible",
+    flag=1
+    }
+    else{
+        document.querySelector("#main6>#box5>h1").style.rotate = "0deg",
+        document.querySelector("#main6>#ans5").style.maxHeight =  "0%",
+        document.querySelector("#main6>#ans5").style.visibility = "hidden",
+        flag=0
+    }
+})
+document.querySelector("#main6>#box6").addEventListener("click",function(){
+    if(flag==0){
+    document.querySelector("#main6>#box6>h1").style.rotate = "45deg",
+    document.querySelector("#main6>#ans6").style.maxHeight =  "30%",
+    document.querySelector("#main6>#ans6").style.visibility = "visible",
+    flag=1
+    }
+    else{
+        document.querySelector("#main6>#box6>h1").style.rotate = "0deg",
+        document.querySelector("#main6>#ans6").style.maxHeight =  "0%",
+        document.querySelector("#main6>#ans6").style.visibility = "hidden",
+        flag=0
+    }
+})
+
+
